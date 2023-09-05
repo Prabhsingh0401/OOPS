6e8caede36988db7d5fe87470b57e6f79575564e
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-09-05 19:26:59 +0530

    Added 2 more Basic Programs

---
+++ CPP Files/MultiplyToVarivables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int x, y, product;
+     
+    cout << "Enter two integers\n";
+    cin >> x >> y;
+     
+    product = x*y;
+    cout << "Product = " << product;
+     
+    return 0;
+}
