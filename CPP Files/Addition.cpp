15924f2c29114ea78db8eb70baf4d613bbed6855
Prableen Singh <130837233+Prabhsingh0401@users.noreply.github.com>
2023-09-06 23:25:43 +0530

    Create Addition.cpp

---
+++ CPP Files/Addition.cpp
@@ -0,0 +1,15 @@
+// program to add two numbers 
+#include<iostream> 
+#include<conio.h>
+Using namespace std;
+Int main ( ) {
+Int a , b , c
+Cout << "Enter the first number";
+Cin >> a;
+Cout << "Enter the second number";
+Cin >> b;
+C = a + b;
+Cout << "Sum" << c;
+Getch( );
+Return 0;
+} 
