5a3d5ea627201d8c7a4e3aaeccfc14e8441e05e0
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-09-09 19:39:40 +0530

    Included Two New Programs

---
+++ CPP Files/ConvertToLowerCase.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main (){
+    char c;
+    cout<<"Enter the Uppercase Alphabet:  ";
+    cin >>c;
+    if (c >= 'A' && c<='Z')
+    {
+        c +=32;
+        cout<<"Lowercase Alphabet:  "<<c;
+    }
+    else 
+    {
+        cout<<"Put a Uppercase Alphabet";
+    }
+    getch();
+    return 0;
+}
---
+++ CPP Files/Sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+/*We have basically created a function here for doing the sum*/
+
+void sum(int a =10, int b = 10, int c = 10)
+{
+    int d;
+    d = a+b+c;
+    cout<<"sum: "<<d;
+}
+
+int main(){
+    cout<<"I am in main""\n\n";
+    sum(10,20,30);
+    getch();
+}
