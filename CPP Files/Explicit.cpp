7421e291cdf6314d665abd268562cb0901e329a0
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-09-11 23:10:33 +0530

    Added Output images and 4 new programs

---
+++ CPP Files/Explicit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    double x = 1.2;
+
+    int sum = (int)x + 1;
+
+    cout<<"Sum : " << sum;
+    getch();
+    return 0;
+}
---
+++ CPP Files/Implicit.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    int x = 10;
+    char y = 'a';
+
+    x = x + y;
+
+    float z = x + 1.0;
+    cout<<"x = "<< x << endl;
+    cout<<"y = "<< y << endl;
+    cout<<"z = "<< z << endl;
+    getch();
+    return 0;
+}
