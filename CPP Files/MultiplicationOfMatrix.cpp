7a96a5f2bff1c9dd487f1653a5ee04cff5d5779d
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-09-18 23:17:04 +0530

    Matrix Multiplication Program

---
+++ CPP Files/MultiplicationOfMatrix.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+    int matone[3][3], mattwo[3][3], matthree[3][3];
+    int i, j, k, sum=0;
+    cout << "Enter 9 elements for matrix";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        cin >> matone[i][j];
+    }
+    cout << "Enter 9 elements for Second Matrix";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        cin >> mattwo[i][j];
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            sum=0;
+            for(k=0;k<3;k++)
+            sum = sum + (matone[i][k]*mattwo[k][j]);
+            matthree[i][j] = sum;
+        }
+    }
+    cout << "Multiplication Result :";
+    cout << "\n";
+    for (i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        cout << matthree[i][j] << "\t";
+        cout << endl;
+    }
+    cout << endl;
+    getch();
+    return 0;
+}
