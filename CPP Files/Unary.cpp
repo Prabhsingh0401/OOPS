9648e0b52a149f0822770fc7417f5db23050874f
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-10-12 23:29:27 +0530

    Create Unary.cpp

---
+++ CPP Files/Unary.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+// Prableen Singh - CSE 3
+
+using namespace std;
+class Distance {
+    public:
+        int feet, inch;
+        Distance(int f, int i) {
+            this->feet = f;
+            this->inch = i;
+        };
+        void operator-() {
+            feet--;
+            inch--;
+            cout << "Feet And Inches (Decrement): " << feet <<"'" << inch;
+        };
+};
+int main() {
+    Distance d1(9, 10);
+    -d1;
+    return 0;
+};
